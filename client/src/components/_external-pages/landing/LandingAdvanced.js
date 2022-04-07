86e91d725c115b6aebf8478d96e4a3afff6630ea
aimachieve <aim1005achieve@gmail.com>
2022-04-08 03:49:14 +0500

    ...

--- client/src/components/_external-pages/landing/LandingAdvanced.js
+++ client/src/components/_external-pages/landing/LandingAdvanced.js
@@ -33,7 +33,7 @@ export default function LandingAdvanced() {
             <Stack justifyContent={'center'} alignItems={'center'} spacing={2}>
               <MotionInView variants={varFadeInUp}>
                 <Typography sx={{
-                  fontSize: {xs: '35px', md: '65px'},
+                  fontSize: { xs: '35px', md: '65px' },
                   letterSpacing: '-1px',
                   color: '#ffffff',
                   fontFamily: "'Michroma', sans-serif",
@@ -54,68 +54,66 @@ export default function LandingAdvanced() {
                   fontFamily: "Roboto",
                   textAlign: 'center',
                 }}>
-                  A collection of 3.000 custom designed NFTs.........insert descriptive copy/sales pitch here.
+                  Para Holdings Inc in conjunction with CryptoFlow101 is proud to present their first release of 3,000 PARA PREPPERS Generative NFT collection.
                   <br />
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  <br />
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  With each purchase from the NFT collection, you will be making an investment into the company with profits going back into $Para to help fund marketing and development.
                 </Typography>
               </MotionInView>
             </Stack>
 
-          <MotionInView variants={varFadeInUp}>
-            <img src="/assets/para/hazard.png" alt="harzard" />
-          </MotionInView>
+            <MotionInView variants={varFadeInUp}>
+              <img src="/assets/para/hazard.png" alt="harzard" />
+            </MotionInView>
 
-          <MotionInView variants={varFadeInUp}>
-            <Grid
-              container
-              justifyContent={{ xs: 'center', md: 'space-around' }}
-              sx={{ textAlign: { xs: 'center', md: 'left' } }}
-            >
-              <Grid item xs={12} md={4}>
-                <img src="/assets/para/jungle.png" alt="jungle" style={{ height: '90%' }} />
+            <MotionInView variants={varFadeInUp}>
+              <Grid
+                container
+                justifyContent={{ xs: 'center', md: 'space-around' }}
+                sx={{ textAlign: { xs: 'center', md: 'left' } }}
+              >
+                <Grid item xs={12} md={4}>
+                  <img src="/assets/para/jungle.png" alt="jungle" style={{ height: '90%' }} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                  <MotionInView variants={varFadeInUp}>
+                    <Stack justifyContent={'center'} >
+                      <Typography
+                        sx={{
+                          fontSize: { xs: '35px', md: '65px' },
+                          letterSpacing: '-1px',
+                          color: '#cddd17',
+                          fontFamily: "'Michroma', sans-serif",
+                          textAlign: { xs: 'center', md: 'left' },
+                          mt: 10,
+                          fontStyle: 'italic',
+                          fontWeight: 'bold'
+                        }}
+                      >
+                        ADVANCED
+                      </Typography>
+                      <Typography
+                        sx={{
+                          fontSize: '16px',
+                          letterSpacing: '0px',
+                          lineHeight: '27px',
+                          color: '#ffffff',
+                          fontWeight: '300',
+                          fontFamily: "Roboto",
+                        }}
+                      >
+                        After leaving the school of hard knocks and basic training on the streets; the Para Preppers were plunged directly into advanced training in the back hills of the Metaverse. With the sound of banjoes in the distance and chiggers running up their legs, the preppers tirelessly train day and night.
+                        <br />
+                        <br />
+                        Having mastered the basics of survival, the Preppers are now in the advanced stages of making explosives from the lethal combination of canned meats and pine tree scented air freshener. Weekly visits to the local flea market and military surplus stores prove to be highly successful in obtaining the much-needed supplies and equipment for their apocalyptic preparations.
+                      </Typography>
+                    </Stack>
+                  </MotionInView>
+                </Grid>
               </Grid>
-              <Grid item xs={12} md={8}>
-                <MotionInView variants={varFadeInUp}>
-                  <Stack justifyContent={'center'} >
-                    <Typography
-                      sx={{
-                        fontSize: {xs: '35px', md: '65px'},
-                        letterSpacing: '-1px',
-                        color: '#cddd17',
-                        fontFamily: "'Michroma', sans-serif",
-                        textAlign: {xs: 'center', md: 'left'},
-                        mt: 10,
-                        fontStyle: 'italic',
-                        fontWeight: 'bold'
-                      }}
-                    >
-                      ADVANCED
-                    </Typography>
-                    <Typography
-                      sx={{
-                        fontSize: '16px',
-                        letterSpacing: '0px',
-                        lineHeight: '27px',
-                        color: '#ffffff',
-                        fontWeight: '300',
-                        fontFamily: "Roboto",
-                      }}
-                    >
-                      After leaving the school of hard knocks and basic training on the streets; the Para Preppers were plunged directly into advanced training in the back hills of the Metaverse. With the sound of banjoes in the distance and chiggers running up their legs, the preppers tirelessly train day and night.
-                      <br />
-                      <br />
-                      Having mastered the basics of survival, the Preppers are now in the advanced stages of making explosives from the lethal combination of canned meats and pine tree scented air freshener. Weekly visits to the local flea market and military surplus stores prove to be highly successful in obtaining the much-needed supplies and equipment for their apocalyptic preparations.
-                    </Typography>
-                  </Stack>
-                </MotionInView>
-              </Grid>
-            </Grid>
-          </MotionInView>
-        </Stack>
-      </ContentStyle>
-    </Container>
+            </MotionInView>
+          </Stack>
+        </ContentStyle>
+      </Container>
     </RootStyle >
   );
 }
