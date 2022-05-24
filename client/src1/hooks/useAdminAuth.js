46daecfa1953b950d67ea74d871f504dcbb6fae5
aimachieve <aim1005achieve@gmail.com>
2022-05-24 15:14:48 +0500

    for Artem

--- client/src1/hooks/useAdminAuth.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AdminAuthContext } from '../contexts/AdminAuthContext';
-
-const useAdminAuth = () => useContext(AdminAuthContext);
-
-export default useAdminAuth;
--- client/src1/utils/MetamaskErrorMessage.js
+++
@@ -1,6 +0,0 @@
-export const MetamaskErrorMessage = (error) => {
-  const message = error.data
-    ? error.data.message
-    : "execution reverted: Unknown error";
-  return message.slice(20);
-};
